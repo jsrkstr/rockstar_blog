fdf99667f308e47fd3f55119b525dc78e07eaa7e
jsrockstar <sachin.saluja@one97.net>
2012-10-31 23:22:21 +0530

    fixed archive

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -46,6 +46,37 @@ App = {
 
         Backbone.history.start({ pushState : true });
         // Resize();
+
+        this.hijackLink();
+
+    },
+
+
+    hijackLink : function(){
+        // Use absolute URLs  to navigate to anything not in your Router.
+
+        // Only need this for pushState enabled browsers
+        if (Backbone.history && Backbone.history._hasPushState) {
+
+          // Use delegation to avoid initial DOM selection and allow all matching elements to bubble
+          $(document).delegate("a", "click", function(evt) {
+            // Get the anchor href and protcol
+            var href = $(this).attr("href");
+            var protocol = this.protocol + "//";
+
+            // Ensure the protocol is not part of URL, meaning its relative.
+            // Stop the event bubbling to ensure the link will not cause a page refresh.
+            if (href.slice(protocol.length) !== protocol) {
+              evt.preventDefault();
+
+              // Note by using Backbone.history.navigate, router events will not be
+              // triggered.  If this is a problem, change this to navigate on your
+              // router.
+              Backbone.history.navigate(href, true);
+            }
+          });
+
+        }
     }
 
 };
